c54df06254f4e1ec63df6dfe320f07999bae8a6a
nguyethy002 <minhthy0120032003@gmail.com>
2021-06-29 20:59:35 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Cascade Energy take home assignment
+Assignment version: 1
+
+## Overview
+
+Tech stack: `NodeJS`, `ExpressJS`, `Typescript`
+
+## Prerequisite
+
+Make sure you have installed:
+1. [Node](https://nodejs.org/en/)
+2. A terminal ([Git bash](https://git-scm.com/downloads) is recommended)
+
+## Quick start
+
+Once everything required are installed, open the working directory with your terminal then
+1. Run `npm install` to install the required Node's libraries
+2. Once it finished, run `npm start` to start the server
+3. Open up the browser, use `http://localhost:8080/` and if there is no error, that means the server is working
+5. To get the number for the days that have AC and heater turned on at least 1, use `http://localhost:8080/report`
+
+## Code structure
+1. The solution is inside `src/solution.ts`
+2. The output folder for compiled JS files is `dist`
---
+++ src/solution.ts
@@ -0,0 +1,96 @@
+import express from "express";
+import csv from "csvtojson";
+import dayjs from "dayjs";
+
+interface ArrayHistoryData {
+  Name: string;
+  "Date time": string;
+  "Maximum Temperature": string;
+  "Minimum Temperature": string;
+  Temperature: string;
+  "Wind Chill": string;
+  "Heat Index": string;
+  Precipitation: string;
+  Snow: string;
+  "Snow Depth": string;
+  "Wind Speed": string;
+  "Wind Direction": string;
+  "Wind Gust": string;
+  Visibility: string;
+  "Cloud Cover": string;
+  "Relative Humidity": string;
+  Conditions: string;
+}
+
+interface QueryDate {
+  startDate: string;
+  endDate: string;
+}
+const app = express();
+const port = 8080; // default port to listen
+
+const csvFilePath = __dirname + "/history_data_hourly.csv";
+const DATE_FORMAT = "MM-DD-YYYY H:mm:ss";
+const AC_THRESHOLD = 75;
+const HEATER_THRESHOLD = 62;
+
+// define a route handler for the default home page
+app.get("/", async (req, res) => {
+  let counterAc = 0;
+  let counterHeat = 0;
+
+  const query = req.query as unknown as QueryDate;
+
+  const startDate = dayjs(query.startDate, DATE_FORMAT);
+  const endDate = dayjs(query.endDate, DATE_FORMAT);
+
+  const csvData: ArrayHistoryData[] = await csv().fromFile(csvFilePath);
+
+  for (const historyDatum of csvData) {
+    const currentDate = dayjs(historyDatum["Date time"], DATE_FORMAT);
+    const currentTemp = parseFloat(historyDatum.Temperature);
+
+    if (startDate <= currentDate && currentDate <= endDate) {
+      if (!currentTemp) {
+        console.log("No temperature was entered");
+      } else if (currentTemp > AC_THRESHOLD) {
+        counterAc++;
+      } else if (currentTemp < HEATER_THRESHOLD) {
+        counterHeat++;
+      }
+    }
+  }
+
+  res.send(
+    `<h1> Total heat counts in the enetered time range: ${counterHeat} </h1> <h1> Total ac counts in the enetered time range: ${counterAc} </h1> `
+  );
+});
+
+app.get("/report", async (req, res) => {
+  const csvData: ArrayHistoryData[] = await csv().fromFile(csvFilePath);
+  // Look up map to store counter for date that turns on AC/Heater at least 1 time
+  const dateLookupMap: Record<string, boolean> = {};
+  let dateCounter = 0;
+
+  csvData.forEach((historyDatum) => {
+    const curDate = dayjs(historyDatum["Date time"], DATE_FORMAT);
+    const curDay = curDate.date();
+
+    const currentTemp = parseFloat(historyDatum.Temperature);
+    if (currentTemp > AC_THRESHOLD || currentTemp < HEATER_THRESHOLD) {
+      if (!dateLookupMap[curDay]) {
+        dateLookupMap[curDay] = true;
+        dateCounter++;
+      }
+    }
+  });
+
+  res.send(
+    `<h1> Number of date with AC or Heater turned on at least once for the whole dates: ${dateCounter} </h1>`
+  );
+});
+
+// start the Express server
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
